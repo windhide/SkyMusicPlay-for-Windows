a5c1675796d0f06025a49a2dfe5212f83b1ac672
windhide <zhukeyuan@mx.com>
2024-12-06 16:08:30 +0800

    [M] 针对打包进行更改 v2

--- sky-music-web/vue.config.js
+++ sky-music-web/vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-    transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  pluginOptions: {
+    electronBuilder: {
+      appId: 'com.windhide.sky',
+      productName: 'Sky_Music',
+      directories: {
+        output: 'dist_electron',
+      },
+      // 将 icon 配置移到这里
+      build: {
+        win: {
+          icon: 'public/favicon.ico', // 确保图标路径正确
+        },
+      },
+    },
+  },
+});
