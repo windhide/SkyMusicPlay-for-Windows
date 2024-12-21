7ed7008a8a10231b97bd6d9bc1e4414916f804a7
WindHide <WindHide520@gmail.com>
2024-12-21 12:20:34 +0800

    [M] version Update & 摘除跟弹功能冗余标签

--- sky-music-web/src/renderer/src/views/tutorial_keyboard.vue
+++ sky-music-web/src/renderer/src/views/tutorial_keyboard.vue
@@ -3,35 +3,30 @@
     <n-space>
       <n-button
         class="buttonStyle"
-        :color="isActive_Y"
         :type="isGhost_Y ? '' : 'primary'"
         :ghost="isGhost_Y"
         >Y</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_U"
         :type="isGhost_U ? '' : 'primary'"
         :ghost="isGhost_U"
         >U</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_I"
         :type="isGhost_I ? '' : 'primary'"
         :ghost="isGhost_I"
         >I</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_O"
         :type="isGhost_O ? '' : 'primary'"
         :ghost="isGhost_O"
         >O</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_P"
         :type="isGhost_P ? '' : 'primary'"
         :ghost="isGhost_P"
         >P</n-button
@@ -40,35 +35,30 @@
     <n-space style="margin-top: 10px">
       <n-button
         class="buttonStyle"
-        :color="isActive_H"
         :type="isGhost_H ? '' : 'primary'"
         :ghost="isGhost_H"
         >H</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_J"
         :type="isGhost_J ? '' : 'primary'"
         :ghost="isGhost_J"
         >J</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_K"
         :type="isGhost_K ? '' : 'primary'"
         :ghost="isGhost_K"
         >K</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_L"
         :type="isGhost_L ? '' : 'primary'"
         :ghost="isGhost_L"
         >L</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_LL"
         :type="isGhost_LL ? '' : 'primary'"
         :ghost="isGhost_LL"
         >;</n-button
@@ -77,35 +67,30 @@
     <n-space style="margin-top: 10px">
       <n-button
         class="buttonStyle"
-        :color="isActive_N"
         :type="isGhost_N ? '' : 'primary'"
         :ghost="isGhost_N"
         >N</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_M"
         :type="isGhost_M ? '' : 'primary'"
         :ghost="isGhost_M"
         >M</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_BB"
         :type="isGhost_BB ? '' : 'primary'"
         :ghost="isGhost_BB"
         >,</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_NN"
         :type="isGhost_NN ? '' : 'primary'"
         :ghost="isGhost_NN"
         >.</n-button
       >
       <n-button
         class="buttonStyle"
-        :color="isActive_MM"
         :type="isGhost_MM ? '' : 'primary'"
         :ghost="isGhost_MM"
         >/</n-button
@@ -117,47 +102,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { sendData } from '@renderer/utils/fetchUtils'
-
-// "#63e2b7" 成功按下
-// "" 无关按键
-// 正确按键
-// "#e88080" 按错了的按钮
-
-const isActive_Y: any = ref('')
-const isActive_U: any = ref('')
-const isActive_I: any = ref('')
-const isActive_O: any = ref('')
-const isActive_P: any = ref('')
-const isActive_H: any = ref('')
-const isActive_J: any = ref('')
-const isActive_K: any = ref('')
-const isActive_L: any = ref('')
-const isActive_LL: any = ref('')
-const isActive_N: any = ref('')
-const isActive_M: any = ref('')
-const isActive_BB: any = ref('')
-const isActive_NN: any = ref('')
-const isActive_MM: any = ref('')
-const button = [
-  'isActive_Y',
-  'isActive_U',
-  'isActive_I',
-  'isActive_O',
-  'isActive_P',
-  'isActive_H',
-  'isActive_J',
-  'isActive_K',
-  'isActive_L',
-  'isActive_LL',
-  'isActive_N',
-  'isActive_M',
-  'isActive_BB',
-  'isActive_NN',
-  'isActive_MM'
-]
-// '' 为正确按钮
-// true为其他按钮
-
 const isGhost_Y: any = ref(true)
 const isGhost_U: any = ref(true)
 const isGhost_I: any = ref(true)
@@ -248,7 +192,6 @@ window.addEventListener('beforeunload', logout)
 function getNextKey(pass) {
   // 重置所有状态
   ghostButton.forEach((char) => eval(char + '.value = true'))
-  button.forEach((char) => eval(char + ".value = ''"))
   originalSheet.clear()
   pressedKeys.clear()
 
