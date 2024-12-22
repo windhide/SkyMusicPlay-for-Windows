d1fd5444e4620077a9525ddeda116227763303ea
WindHide <WindHide520@gmail.com>
2024-12-22 23:20:05 +0800

    [A] 转换谱子处添加删除功能 detail

--- sky-music-web/src/renderer/src/views/kube.vue
+++ sky-music-web/src/renderer/src/views/kube.vue
@@ -1,6 +1,8 @@
 <template>
   <n-flex align="center">
-    <n-gradient-text :size="24" type="success" style="width: 100%"> 转换歌曲 </n-gradient-text>
+    <n-gradient-text :size="24" type="success" style="width: 100%">
+      转换歌曲
+    </n-gradient-text>
     <n-upload
       action="http://localhost:9899/fileUpload"
       multiple
@@ -19,8 +21,8 @@
       当前任务
       {{
         now_translate_text.process == undefined && now_translate_text.text == undefined
-          ? '已完成'
-          : now_translate_text.process + ' ' + now_translate_text.text
+          ? "已完成"
+          : now_translate_text.process + " " + now_translate_text.text
       }}
     </n-gradient-text>
     <div style="width: 100%">
@@ -49,7 +51,7 @@
     <n-tabs type="line" animated @update:value="handleUpdateValue">
       <n-tab-pane name="translateOriginalMusic" tab="未转换歌曲">
         <n-data-table
-          :columns="musicColumns"
+          :columns="originalColumns"
           :data="music.translateOriginalMusic"
           :bordered="false"
           :max-height="330"
@@ -58,7 +60,7 @@
       </n-tab-pane>
       <n-tab-pane name="myTranslate" tab="已转换歌曲">
         <n-data-table
-          :columns="musicColumns"
+          :columns="translateColumns"
           :data="music.myTranslate"
           :bordered="false"
           :max-height="300"
@@ -70,80 +72,154 @@
 </template>
 
 <script lang="ts" setup>
-import { getData, sendData, getList } from '@renderer/utils/fetchUtils'
-import { reactive, ref, watch } from 'vue'
-import { useMessage } from 'naive-ui'
-const message = useMessage()
-const processFlag = ref(false)
+import { getData, sendData, getList } from "@renderer/utils/fetchUtils";
+import { h, reactive, ref, watch } from "vue";
+import { NButton, useMessage } from "naive-ui";
+const message = useMessage();
+const processFlag = ref(false);
 
 const music: any = reactive({
   // 音乐列表
   translateOriginalMusic: [], // 导入的音乐
-  myTranslate: [] // 扒谱的音乐
-})
+  myTranslate: [], // 扒谱的音乐
+});
 const progress: any = reactive({
   translate_progress: 0,
-  overall_progress: 0
-})
+  overall_progress: 0,
+});
 const now_translate_text = reactive({
-  process: '',
-  text: ''
-})
+  process: "",
+  text: "",
+});
 
-const musicColumns = [
+const originalColumns = [
   {
-    title: '歌名',
-    key: 'name'
-  }
-] // 音乐列
+    title: "歌名",
+    key: "name",
+  },
+  {
+    title: "操作",
+    key: "operation",
+    width: 100,
+    render(row) {
+      return h(
+        NButton,
+        {
+          size: "medium",
+          text: false,
+          onClick: () => originalClick(row.name),
+        },
+        {
+          default: () => {
+            return "❌";
+          },
+        }
+      );
+    },
+  },
+]; // 音乐列
 
-function handleFinish() {
-  reloadTable()
-  message.success('OK~')
+const translateColumns = [
+  {
+    title: "歌名",
+    key: "name",
+  },
+  {
+    title: "操作",
+    key: "operation",
+    width: 100,
+    render(row) {
+      return h(
+        NButton,
+        {
+          size: "medium",
+          text: false,
+          onClick: () => translateClick(row.name),
+        },
+        {
+          default: () => {
+            return "❌";
+          },
+        }
+      );
+    },
+  },
+]; // 音乐列
+
+function originalClick(name) {
+    sendData("dropFile", {
+      fileName: name.slice(0, name.lastIndexOf('.')),
+      type: 'translateOriginalMusic',
+      suffix: name.match(/(\.[^.]*)$/)[0]
+    }).then(() => {
+      handleUpdateValue("translateOriginalMusic");
+      message.success("移除成功");
+    });
 }
 
-let progressInterval: any
-function getProgress() {
-  getData('getProgress').then((res) => {
-    progress.tran_mid_progress = res.tran_mid_progress
-    progress.overall_progress = res.overall_progress
-    now_translate_text.text = res.now_translate_text[0]
-    now_translate_text.process = res.now_translate_text[1]
-  })
+function translateClick(name) {
+    sendData("dropFile", {
+      fileName: name,
+      type: 'myTranslate',
+    }).then(() => {
+      handleUpdateValue("translateOriginalMusic");
+      handleUpdateValue("myTranslate");
+      message.success("移除成功");
+    });
+    sendData("dropFile", {
+      fileName: name,
+      type: 'translateMID',
+      suffix: '.mid'
+    })
+}
 
-  if (progress.tran_mid_progress == '100.0' && progress.overall_progress == '100.0') {
-    clearInterval(progressInterval)
+function handleFinish() {
+  reloadTable();
+  message.success("OK~");
+}
+
+let progressInterval: any;
+function getProgress() {
+  getData("getProgress").then((res) => {
+    progress.tran_mid_progress = res.tran_mid_progress;
+    progress.overall_progress = res.overall_progress;
+    now_translate_text.text = res.now_translate_text[0];
+    now_translate_text.process = res.now_translate_text[1];
+  });
+
+  if (progress.tran_mid_progress == "100.0" && progress.overall_progress == "100.0") {
+    clearInterval(progressInterval);
   }
 }
 
 function handleStartTranslate() {
-  if (processFlag.value) return
-  progressInterval = setInterval(getProgress, 1000)
-  processFlag.value = true
-  message.success('开始转换')
-  sendData('translate', {
-    processor: 'cpu'
+  if (processFlag.value) return;
+  progressInterval = setInterval(getProgress, 1000);
+  processFlag.value = true;
+  message.success("开始转换");
+  sendData("translate", {
+    processor: "cpu",
   })
     .then(() => {
-      reloadTable()
-      message.success('转换完成')
+      reloadTable();
+      message.success("转换完成");
     })
     .finally(() => {
-      processFlag.value = false
-    })
+      processFlag.value = false;
+    });
 }
 
 function reloadTable() {
-  handleUpdateValue('myTranslate')
-  handleUpdateValue('translateOriginalMusic')
+  handleUpdateValue("myTranslate");
+  handleUpdateValue("translateOriginalMusic");
 }
 
-reloadTable()
-watch(progress.overall_progress.value, () => reloadTable())
+reloadTable();
+watch(progress.overall_progress.value, () => reloadTable());
 
 function handleUpdateValue(value: string) {
-  getList(value, '').then((_res) => {
-    eval('music.' + value + '=_res')
-  })
+  getList(value, "").then((_res) => {
+    eval("music." + value + "=_res");
+  });
 }
 </script>
