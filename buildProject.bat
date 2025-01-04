950fa60d42c251973e60966f51fe98dac39f5632
WindHide <WindHide520@gmail.com>
2025-01-04 15:38:26 +0800

    [M] 打包改动提交

--- buildProject.bat
+++ buildProject.bat
@@ -29,34 +29,15 @@ rmdir /S /Q "%script_dir%sky-music-web\dist"
 :: 删除python的构建目录
 rmdir /S /Q "%script_dir%sky-music-server\build"
 
-:: 构建 Electron 应用
-cd "%script_dir%sky-music-web"
-call npm run build:win
-
 :: 构建 Python 服务器
 cd "%script_dir%sky-music-server"
 call .venv\Scripts\activate
-call pyinstaller --uac-admin -w sky-music-server.py --distpath "%script_dir%sky-music-web\dist\win-unpacked\backend_dist" --hidden-import=main --collect-all=sklearn
+call pyinstaller --uac-admin -w sky-music-server.py --distpath "%script_dir%sky-music-web\backend_dist" --hidden-import=main --collect-all=sklearn
 call deactivate
 
-:: 复制 ffmpeg 可执行文件
-copy "%script_dir%ffmpeg.exe" "%script_dir%sky-music-web\dist\win-unpacked\backend_dist\sky-music-server\ffmpeg.exe"
-
-:: 设置源路径和目标路径
-set source="%script_dir%template-resources"
-set destination="%script_dir%sky-music-web\dist\win-unpacked\resources"
-
-:: 使用 robocopy 复制文件并保留属性
-echo Copying files with robocopy...
-robocopy %source% %destination% /E /Z /COPYALL /R:3 /W:5
-
-:: 删除目标文件夹下的所有 .gitkeep 文件
-echo Cleaning up .gitkeep files...
-for /r "%destination%" %%f in (*.gitkeep) do (
-    del /f /q "%%f"
-)
-
-echo File copy and cleanup completed!
+:: 构建 Electron 应用
+cd "%script_dir%sky-music-web"
+call npm run build:win
 
 :: 完成提示
 echo.
