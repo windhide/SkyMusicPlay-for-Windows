48a06351f7ae3eff979ba8217762289e7fa005c6
WindHide <WindHide520@gmail.com>
2025-02-05 16:52:27 +0800

    [optimize] 快捷键代码优化

--- sky-music-server/windhide/thread/shortcut_thread.py
+++ sky-music-server/windhide/thread/shortcut_thread.py
@@ -7,41 +7,34 @@ from windhide.utils import hook_util
 
 hook_util.sout_null()
 
-# WebSocket 服务端实例
 server = WebsocketServer("127.0.0.1", 11452)
+
+# 按键映射
+key_mappings = {
+    keyboard.Key.f2: "F2",
+    keyboard.Key.f5: "F5",
+    keyboard.Key.f6: "F6",
+    keyboard.Key.f7: "F7",
+    keyboard.Key.f8: "F8"
+}
+
 # 键盘按键事件处理
 def on_press(key):
-    try:
-        # 仅处理特定的按键
-        if key == keyboard.Key.f5:
-            print("按键 F5 被触发")
-            server.send_message_to_all(urllib.parse.quote("F5"))
-        elif key == keyboard.Key.f6:
-            print("按键 F6 被触发")
-            server.send_message_to_all(urllib.parse.quote("F6"))
-        elif key == keyboard.Key.f7:
-            print("按键 F7 被触发")
-            server.send_message_to_all(urllib.parse.quote("F7"))
-        elif key == keyboard.Key.f8:
-            print("按键 F8 被触发")
-            server.send_message_to_all(urllib.parse.quote("F8"))
-        elif key == keyboard.Key.f2:
-            print("按键 F2 被触发")
-            server.send_message_to_all(urllib.parse.quote("F2"))
-    except AttributeError:
-        return
-    except Exception as e:
-        print(f"发生错误: {e}")
+    if key in key_mappings:
+        key_name = key_mappings[key]
+        print(f"按键 {key_name} 被触发")
+        try:
+            server.send_message_to_all(urllib.parse.quote(key_name))
+        except Exception as e:
+            print(f"发送消息时发生错误: {e}")
 
-# 客户端连接事件处理
+
+# 客户端事件处理
 def on_client_connect(client, server):
     print(f"客户端 {client['id']} 已连接")
-    print(f"{server}")
 
-# 客户端断开事件处理
 def on_client_disconnect(client, server):
     print(f"客户端 {client['id']} 已断开连接")
-    print(f"{server}")
 
 # 启动 WebSocket 服务
 def startThread():
