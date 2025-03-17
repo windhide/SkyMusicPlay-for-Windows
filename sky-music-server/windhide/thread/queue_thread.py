050ff67ea3f5a0c790d7fa6d50b1e6be6b917e3a
WindHide <windhide520@gmail.com>
2025-03-17 19:37:22 +0800

    [M] 播放队列卡进程的问题初步修复，需要详细跟进

--- sky-music-server/windhide/thread/queue_thread.py
+++ sky-music-server/windhide/thread/queue_thread.py
@@ -1,16 +1,25 @@
-import time
+import queue
 
+import plyer
 from windhide.playRobot import intel_robot, amd_robot
 from windhide.static.global_variable import GlobalVariable
 
-
 def music_start_tasks():
     while True:
-        start_time = time.time()
-        request = GlobalVariable.task_queue.get()  # 从队列中取出任务
+        try:
+            request = GlobalVariable.task_queue.get(block=False)  # 从队列中取出任务
+        except queue.Empty:
+            continue
         try:
             if GlobalVariable.window["hWnd"] is None and GlobalVariable.compatibility_mode is False:
-                return
+                plyer.notification.notify(
+                    title='⚠️⚠️⚠️⚠️⚠️',
+                    message='未检测到游戏窗口，请打开游戏或者去句柄页面进行指定！本次播放操作释放',
+                    timeout=3
+                )
+                GlobalVariable.now_progress = 100
+                GlobalVariable.task_queue.task_done()
+                continue
             GlobalVariable.now_play_music = ("" if 'sheet' in request else request["fileName"])
             match GlobalVariable.cpu_type:
                 case "Intel":
@@ -20,5 +29,3 @@ def music_start_tasks():
         except Exception as e:
             print(f"Error in processing task: {str(e)}")
         GlobalVariable.task_queue.task_done()  # 标记任务完成
-        end_time = time.time()
-        print("Time elapsed:", end_time - start_time)
