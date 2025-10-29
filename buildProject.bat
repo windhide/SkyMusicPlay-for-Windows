2a9eee04ffdd496279226bca581592d35faf10f2
WindHide <windhide520@gmail.com>
2025-10-29 17:59:39 +0800

    [M] 打包bat更新

--- buildProject.bat
+++ buildProject.bat
@@ -22,24 +22,24 @@ cd /d "%script_dir%"
 echo Current script directory: %script_dir%
 
 :: 删除旧的 Electron 构建目录
-rmdir /S /Q "%script_dir%\sky-music-web\dist"
+rmdir /S /Q "%script_dir%sky-music-web\dist"
 :: 删除 Python 的构建目录
-rmdir /S /Q "%script_dir%\sky-music-server\build"
-rmdir /S /Q "%script_dir%\sky-music-web\backend_dist"
+rmdir /S /Q "%script_dir%sky-music-server\build"
+rmdir /S /Q "%script_dir%sky-music-web\backend_dist"
 
 :: 构建 Python 服务器
-cd "%script_dir%\sky-music-server"
-call .venv\Scripts\python.exe -m PyInstaller -i icon.ico sky_music_server.py --distpath "%script_dir%\sky-music-web\backend_dist" --version-file=%script_dir%\sky-music-server\version.txt --hidden-import=main --collect-all=sklearn --collect-all=basic_pitch --collect-all=plyer
+cd "%script_dir%sky-music-server"
+call .venv\Scripts\python.exe -m PyInstaller -i icon.ico sky_music_server.py --distpath "%script_dir%sky-music-web\backend_dist" --version-file=%script_dir%sky-music-server\version.txt --hidden-import=main --collect-all=sklearn --collect-all=basic_pitch --collect-all=plyer
 
 :: 构建 跟弹服务
 :: cd "%script_dir%\draw-follow-window"
 :: call .venv\Scripts\python.exe -m pyinstaller --onefile --noconsole --clean --strip --name draw_server --distpath "%script_dir%\template-resources\systemTools\drawTool" draw_server.py
 
 :: 复制 ffmpeg.exe
-copy "%script_dir%\ffmpeg.exe" "%script_dir%\sky-music-web\backend_dist\sky-music-server\ffmpeg.exe"
+copy "%script_dir%ffmpeg.exe" "%script_dir%sky-music-web\backend_dist\sky_music_server\ffmpeg.exe"
 
 :: 构建 Electron 应用
-cd "%script_dir%\sky-music-web"
+cd "%script_dir%sky-music-web"
 call npm run build:win
 
 :: 完成提示
